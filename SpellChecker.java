a723898d1d71746c0a9695ecf9677121feb79711
rotembreslauer <rotem.breslauer@post.runi.ac.il>
2024-02-02 19:58:31 +0200

    2

--- SpellChecker.java
+++ SpellChecker.java
@@ -17,19 +17,19 @@ public class SpellChecker {
 	public static int levenshtein(String word1, String word2) {
 		word1 = word1.toLowerCase();
 		word2 = word2.toLowerCase();
-		int length1 = word1.length();
-		int length2 = word2.length();
-		if (length1 == 0){
-			return length1;
-		} else if(length2 == 0){
-			return length2;
-		}else if(word1.charAt(0) == word2.charAt(0)){
-			return levenshtein(tail(word1), tail(word2));
-		} else {
-			return 1 + Math.min(Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2))), levenshtein(tail(word1), tail(word2)));
+		if (word1.isEmpty()){
+			return word2.length();
 		}
-			
+		if (word2.isEmpty()){
+			return word1.length();
 		}
+		if (word1.charAt(0) == word2.charAt(0)){
+			return  levenshtein(tail(word1), tail(word2));
+		}else{
+			return 1 + Math.min(Math.min(levenshtein(tail(word1), tail(word2)) , levenshtein(word1, tail(word2))) , levenshtein(word2, tail(word1)));
+		}
+	}
+
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
@@ -43,26 +43,22 @@ public class SpellChecker {
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		
-		// the word to return
-		int min = threshold +1;
-		String mostSimiler = word;
-		// this loop runs on the dictionary and checks the distance between the input
-		for (int i = 0; i < dictionary.length; i++) {
-			int distance = levenshtein(word, dictionary[i]);
-			// identifies the word with the minimum distance
-			// checks whether this distance exceeds the given threshold
-			if (distance < min) {
-				min = distance;
-				// if the distance is smaller than the threshold, the word is replaced
-				mostSimiler = dictionary[i];
+		 String mostSimilar = word; 
+
+		for (int j = 0; j <= threshold; j++) {
+			for (int i = 0; i < dictionary.length; i++) {
+			String candidate = dictionary[i];
+			int distance = levenshtein(word.toLowerCase(), candidate);
+	
+				if (distance <= j && distance < levenshtein(word.toLowerCase(), mostSimilar)) {
+					mostSimilar = candidate;
+				}
 			}
+	}
+		if (levenshtein(word.toLowerCase(), mostSimilar) > threshold) {
+		return word;
 		}
-		// if the minimum distance found is bigger than the threshold,
-		// this indicates that no word in the dictionary is sufficiently similar to the
-		// input word.
-		return mostSimiler;
+		return mostSimilar;
 	}
 }
-
 
